3201e459973ae69b12f9f6f5450b424ac0adbf86
Sharunfhnw <sharun.sivaneswaran@students.fhnw.ch>
2026-05-19 17:45:28 +0200

    Add pages __init__ files

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,1 +1,4 @@
 """Pages package for web UI views."""
+from pages.login import login_page
+from pages.register import register_page
+from pages.profile import profile_page
---
+++ pages/student/__init__.py
@@ -0,0 +1,4 @@
+from pages.student.dashboard import student_dashboard
+from pages.student.quiz_view import quiz_view
+from pages.student.results import results_page
+from pages.student.statistics import student_statistics
